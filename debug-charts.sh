d6f43e7f5280dfdfb456e0d8df4868d8588fcd56
Asmund Tokheim <asmtok@arkivverket.no>
2026-01-12 12:22:18 +0100

    More debugging

--- debug-charts.sh
+++ debug-charts.sh
@@ -10,10 +10,7 @@ for path in helm/*/ ; do
         filename=$(basename -- "$configpath")
         config="${filename%.*}"
         ns="$chart-${config%.*}"
-        echo "testing chart $chart w/ cfg $configpath in ns $ns"
-        helm upgrade --install test-app -n $ns helm/$chart -f $configpath --debug --dry-run=server
-        helm upgrade --install test-app -n $ns helm/$chart -f $configpath $args
-        echo "running tests for chart $chart w/ cfg $configpath"
-        helm test test-app -n $ns
+        echo "status chart $chart w/ cfg $configpath in ns $ns"
+        helm status test-app -n $ns
     done
 done
